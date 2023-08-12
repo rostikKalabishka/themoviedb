d79b7aef94534c251f4008843854fed2b16313ed
RostikKalabishka <rostik31052002@gmail.com>
2023-08-12 14:46:58 +0300

    add initialRoute

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       routes: routes,
+      initialRoute: '/auth',
       title: 'Flutter Demo',
       theme: ThemeData(
         appBarTheme:
--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -2,6 +2,8 @@ import '../widgets/auth/auth_widget.dart';
 import '../widgets/main_screen/main_screen_widget.dart';
 
 final routes = {
-  '/': (context) => AuthWidget(),
+  '/auth': (context) => AuthWidget(),
   '/main_screen': (context) => MainScreenWidget(),
+  // '/resend_email': (context) => ResendEmail(),
+  //'/signup':: (context) => Signup()
 };
