b4351ab4f7bd0a1e08305fd853965a85ea70b1af
RostikKalabishka <rostik31052002@gmail.com>
2023-08-16 16:48:50 +0300

    add films screen, add for styling Material, InkWell

--- lib/widgets/main_screen/movie_list/movie_list_widget.dart
+++ lib/widgets/main_screen/movie_list/movie_list_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:themoviedb/resources/resources.dart';
 
 class MovieListWidget extends StatelessWidget {
   const MovieListWidget({super.key});
@@ -9,8 +10,66 @@ class MovieListWidget extends StatelessWidget {
         itemCount: 10,
         itemExtent: 163,
         itemBuilder: (BuildContext context, int index) {
-          return Container(
-            color: Colors.red,
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+            child: Stack(children: [
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(color: Colors.black.withOpacity(0.2)),
+                    borderRadius: const BorderRadius.all(Radius.circular(10)),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black.withOpacity(0.1),
+                          blurRadius: 8,
+                          offset: const Offset(0, 2)),
+                    ]),
+                clipBehavior: Clip.hardEdge,
+                child: const Row(
+                  children: [
+                    Image(
+                      image: AssetImage(AppImages.spiderMan),
+                    ),
+                    SizedBox(width: 15),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(height: 20),
+                          Text(
+                            'Spider-Man',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          SizedBox(height: 5),
+                          Text(
+                            'Aug 16, 2023',
+                            style: TextStyle(color: Colors.grey),
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          SizedBox(height: 20),
+                          Text(
+                            'Цього разу на Майлза Моралеса чекає несподівана подорож крізь всесвіт, у якій він об’єднає зусилля з Ґвен Стейсі та новою командою «павуків», щоби протистояти наймогутнішому лиходію, котрого вони коли-небудь зустрічали.',
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                          )
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                  ],
+                ),
+              ),
+              Material(
+                color: Colors.transparent,
+                child: InkWell(
+                  borderRadius: const BorderRadius.all(Radius.circular(10)),
+                  onTap: () {},
+                ),
+              )
+            ]),
           );
         });
   }
